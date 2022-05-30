81ac7776cd73f27f67699c1c78dbe841079b8028
Kilian <kilian1103@hotmail.de>
2022-05-30 14:12:36 +0100

    refactor project structure to module (2)

---
+++ NSLFI/MCMCSampler.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+
+
+class Sampler:
+
+    def __init__(self, prior, logLikelihood, ndim):
+        self.prior = prior
+        self.logLikelihood = logLikelihood
+        self.ndim = ndim
+        self.samplers = {"Metropolis": Metropolis,
+                         "Rejection": Rejection}
+
+    def getSampler(self, type):
+        return self.samplers[type](prior=self.prior, logLikelihood=self.logLikelihood, ndim=self.ndim)
+
+
+class Metropolis(Sampler):
+    def __init__(self, prior, logLikelihood, ndim):
+        super().__init__(prior=prior, logLikelihood=logLikelihood, ndim=ndim)
+
+    def sample(self, minlogLike, livepoints, nrepeat=5) -> np.ndarray:
+        cov = np.cov(livepoints.T)
+        random_index = np.random.randint(0, len(livepoints))
+        current_sample = livepoints[random_index].copy()
+        for i in range(nrepeat * self.ndim):
+            proposal_sample = multivariate_normal.rvs(mean=current_sample, cov=cov)
+            withinPrior = np.logical_and(proposal_sample > 0, proposal_sample < 1).all()
+            withinContour = self.logLikelihood(proposal_sample, self.ndim) > minlogLike
+            if withinPrior and withinContour:
+                current_sample = proposal_sample.copy()
+        return current_sample
+
+
+class Rejection(Sampler):
+    def __init__(self, prior, logLikelihood, ndim):
+        super().__init__(prior=prior, logLikelihood=logLikelihood, ndim=ndim)
+
+    def sample(self, minlogLike, **kwargs) -> np.ndarray:
+        while True:
+            proposal_sample = self.prior(self.ndim, 1)[0]
+            if self.logLikelihood(proposal_sample, self.ndim) > minlogLike:
+                break
+        return proposal_sample
