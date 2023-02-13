8d9dca3cb932f3be5dd424e41b81932a93065e14
Kilian <kilian1103@hotmail.de>
2023-02-13 17:38:17 +0000

    implement 1d slice sampler + test

--- NSLFI/MCMCSampler.py
+++ NSLFI/MCMCSampler.py
@@ -11,7 +11,8 @@ class Sampler:
         self.logLikelihood = logLikelihood
         self.ndim = len(prior)
         self.samplers = {"Metropolis": Metropolis,
-                         "Rejection": Rejection}
+                         "Rejection": Rejection,
+                         "Slice": Slice}
 
     def getSampler(self, type):
         return self.samplers[type](prior=self.prior, logLikelihood=self.logLikelihood)
@@ -58,3 +59,48 @@ class Rejection(Sampler):
             if self.logLikelihood(proposal_sample) > minlogLike:
                 break
         return proposal_sample
+
+
+class Slice(Sampler):
+    def __init__(self, prior: Dict[str, Any], logLikelihood: Any):
+        super().__init__(prior=prior, logLikelihood=logLikelihood)
+
+    def sample(self, minlogLike, livepoints, livelikes, nrepeat=5, step_size=0.1) -> np.ndarray:
+        lower = np.zeros(self.ndim)
+        upper = np.zeros(self.ndim)
+        for i, val in enumerate(self.prior.values()):
+            low, up = val.support()
+            lower[i] = low
+            upper[i] = up
+
+        i = np.arange(len(livelikes))[livelikes > minlogLike]
+        random_index = np.random.choice(i)
+        current_sample = livepoints[random_index].copy()
+        intermediate_sample = current_sample.copy()
+        x_l, x_r = self._extend_1d_interval(intermediate_sample, step_size, minlogLike)
+        for i in range(nrepeat * self.ndim):
+            x_n_new = np.random.uniform(x_l, x_r)
+            intermediate_sample = x_n_new
+            withinPrior = np.logical_and(np.greater(intermediate_sample, lower),
+                                         np.less(intermediate_sample, upper)).all()
+            withinContour = self.logLikelihood(intermediate_sample) > minlogLike
+            if withinPrior and withinContour:
+                current_sample = intermediate_sample.copy()
+                x_l, x_r = self._extend_1d_interval(current_sample, step_size, minlogLike)
+            else:
+                if intermediate_sample > current_sample:
+                    x_r = intermediate_sample
+                else:
+                    x_l = intermediate_sample
+
+        return current_sample
+
+    def _extend_1d_interval(self, current_sample, step_size, minlogLike):
+        r = np.random.uniform(0, 1)
+        x_l = current_sample - r * step_size
+        x_r = current_sample + (1 - r) * step_size
+        while self.logLikelihood(x_l) > minlogLike:
+            x_l -= step_size
+        while self.logLikelihood(x_r) > minlogLike:
+            x_r += step_size
+        return x_l, x_r
--- tests/test_nested_sampler.py
+++ tests/test_nested_sampler.py
@@ -68,3 +68,33 @@ def test_nested_sampler_rejection():
     os.remove("newPoints.npy")
     os.remove("posterior_samples.npy")
     os.remove("weights.npy")
+
+
+def test_nested_sampler_slice():
+    """
+    Test Nested sampling run on Gaussian likelihood using a slice sampler.
+    The analytical result for this problem with an uniform (0,1) prior is log Z = 0 for any dimension n.
+    """
+    np.random.seed(234)
+
+    ndim = 1
+    nlive = 100
+
+    def logLikelihood(x) -> np.ndarray:
+        # Multivariate Gaussian centred at X = 0.5, y= 0.5
+        means = 0.5 * np.ones(shape=ndim)
+        cov = 0.01 * np.eye(N=ndim)
+        return multivariate_normal.logpdf(x=x, mean=means, cov=cov)
+
+    priors = {f"theta_{i}": stats.uniform(loc=0, scale=1) for i in range(ndim)}
+    livepoints = priors["theta_0"].rvs(size=(nlive, ndim))
+
+    logZ = nested_sampling(logLikelihood=logLikelihood, prior=priors,
+                           nsim=100, stop_criterion=1e-3, livepoints=livepoints, samplertype="Slice")
+    print(logZ)
+    np.testing.assert_almost_equal(actual=logZ["log Z mean"], desired=0, decimal=0.2)
+    os.remove("logL.npy")
+    os.remove("logL_birth.npy")
+    os.remove("newPoints.npy")
+    os.remove("posterior_samples.npy")
+    os.remove("weights.npy")
