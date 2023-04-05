14acfa46ee01f88c242485b94522abfad9f9fea2
Kilian <kilian1103@hotmail.de>
2023-04-05 16:08:17 +0100

    minor tidy up code

--- main_banana_torch.py
+++ main_banana_torch.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from typing import Dict, Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,6 +11,7 @@ from swyft import collate_output
 
 import NSLFI.NestedSamplerTorch
 from NSLFI.NRE_Settings import NRE_Settings
+from NSLFI.Swyft_NRE_Wrapper import NRE
 
 
 # from NSLFI.Swyft_NRE_Wrapper import NRE
@@ -119,24 +119,6 @@ def execute():
     swyft.corner(predictions, ["z[0]", "z[1]"], bins=50, smooth=1)
     plt.savefig(f"{root}/NRE_predictions.pdf")
     plt.show()
-
-    class NRE:
-        def __init__(self, network: swyft.SwyftModule, prior: Dict[str, Any],
-                     nreSettings: NRE_Settings, obs: swyft.Sample, livepoints: torch.tensor):
-            self.network = network.eval()
-            self.livepoints = livepoints
-            self.prior = prior
-            self.nre_settings = nreSettings
-            self.obs = {"x": torch.tensor(obs["x"]).type(torch.float64).unsqueeze(0)}
-
-        def logLikelihood(self, proposal_sample: torch.tensor):
-            # check if list of datapoints or single datapoint
-            if proposal_sample.ndim == 1:
-                prediction = self.network(self.obs, {"z": proposal_sample.type(torch.float64)})
-                return prediction.logratios
-            else:
-                prediction = self.network(self.obs, {"z": proposal_sample.type(torch.float64)})
-                return prediction.logratios[:, 0]
 
     # wrap NRE object
     trained_NRE = NRE(network=network, prior=prior, nreSettings=nreSettings, obs=obs,
