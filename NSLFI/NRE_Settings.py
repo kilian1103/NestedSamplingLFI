77e8789d9d8ce9f7dd0f2126b3d9f1d8df1325ad
kilian1103 <kilian1103hotmail.de>
2023-09-19 12:06:17 +0100

    change triangle plots default

--- NSLFI/NRE_Settings.py
+++ NSLFI/NRE_Settings.py
@@ -32,7 +32,7 @@ class NRE_Settings:
         self.prior_width = 2
         self.anesthetic_sample_threshold = 0.999
         self.plot_triangle_plot = True
-        self.plot_triangle_plot_ext = True
+        self.plot_triangle_plot_ext = False
         self.plot_KL_divergence = True
         # polychord settings
         self.nlive_scan_run_per_feature = 25
