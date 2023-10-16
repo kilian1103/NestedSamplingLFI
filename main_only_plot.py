968a6857cc5b1c5848ef1426176bd0b25cf2e33b
kilian1103 <kilian1103hotmail.de>
2023-10-16 17:47:37 +0100

    add quantile plot to plotting script

--- main_only_plot.py
+++ main_only_plot.py
@@ -1,6 +1,7 @@
+from anesthetic import read_chains
 from mpi4py import MPI
 
-from NSLFI.NRE_Post_Analysis import plot_NRE_posterior
+from NSLFI.NRE_Post_Analysis import plot_NRE_posterior, plot_quantile_plot
 from NSLFI.utils import reload_data_for_plotting
 
 
@@ -12,6 +13,13 @@ def main():
         root_storage, network_storage, nreSettings, dataEnv = reload_data_for_plotting()
         plot_NRE_posterior(nreSettings=nreSettings, network_storage=network_storage, root_storage=root_storage,
                            dataEnv=dataEnv)
+
+        for i in range(nreSettings.num_features):
+            root = root_storage[f"round_{i}"]
+            samples = read_chains(root=f"{root}/{nreSettings.file_root}")
+            samples = samples.iloc[:, :nreSettings.num_features]
+            plot_quantile_plot(samples=samples, percentiles=nreSettings.percentiles_of_quantile_plot,
+                               nreSettings=nreSettings, root=root)
 
 
 if __name__ == '__main__':
