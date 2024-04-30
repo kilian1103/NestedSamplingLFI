7b9f64a36442c7af6ff8ecabe4c087595efb4dd7
kilian1103 <kilian1103hotmail.de>
2024-04-30 17:26:10 +0200

    bugfix MVG script

--- main_MVG.py
+++ main_MVG.py
@@ -95,54 +95,6 @@ def execute():
     if rank_gen == 0:
         # plot analysis of NSNSRE
         plot_analysis_of_NSNRE(nreSettings=nreSettings, network_storage=network_storage,
-                               root_storage=root_storage, samples_storage=samples_storage, dkl_storage=dkl_storage,
-                               obs=obs, true_posterior=mcmc_true)
-    logger.info('Finished')
-    ### generate true posterior for comparison
-    cond = {nreSettings.obsKey: obs[nreSettings.obsKey].numpy().squeeze()}
-    full_joint = sim.sample(nreSettings.n_weighted_samples, conditions=cond)
-    true_logratios = torch.as_tensor(full_joint[nreSettings.contourKey])
-    posterior = full_joint[nreSettings.posteriorsKey]
-    weights = np.ones(shape=len(posterior))  # direct samples from posterior have weights 1
-    params_labels = {i: rf"${nreSettings.targetKey}_{i}$" for i in range(nreSettings.num_features)}
-    mcmc_true = MCMCSamples(
-        data=posterior, weights=weights.squeeze(),
-        logL=true_logratios, labels=params_labels)
-
-    #### instantiate swyft network
-    network = Network(nreSettings=nreSettings, obs=obs)
-
-    #### create callbacks function for pytorch lightning trainer
-    def create_callbacks() -> list:
-        early_stopping_callback = EarlyStopping(monitor='val_loss', min_delta=0.,
-                                                patience=nreSettings.early_stopping_patience, mode='min')
-        lr_monitor = LearningRateMonitor(logging_interval='step')
-        checkpoint_callback = ModelCheckpoint(monitor='val_loss',
-                                              filename='NRE_{epoch}_{val_loss:.2f}_{train_loss:.2f}', mode='min')
-        return [early_stopping_callback, lr_monitor, checkpoint_callback]
-
-    #### set up polychord settings
-    polyset = pypolychord.PolyChordSettings(nreSettings.num_features, nDerived=nreSettings.nderived)
-    polyset.file_root = "samples"
-    polyset.base_dir = nreSettings.root
-    polyset.seed = nreSettings.seed
-    polyset.nfail = nreSettings.nlives_per_dim_constant * nreSettings.n_prior_sampling
-    polyset.nprior = nreSettings.n_prior_sampling
-    polySwyft = PolySwyft(nreSettings=nreSettings, sim=sim, obs=obs, deadpoints=training_samples,
-                          network=network, polyset=polyset, callbacks=create_callbacks)
-
-    if not nreSettings.only_plot_mode:
-        ### execute main cycle of NSNRE
-        polySwyft.execute_NSNRE_cycle()
-
-    root_storage, network_storage, samples_storage, dkl_storage = reload_data_for_plotting(nreSettings=nreSettings,
-                                                                                           network=network,
-                                                                                           polyset=polyset,
-                                                                                           until_round=nreSettings.NRE_num_retrain_rounds)
-
-    if rank_gen == 0:
-        # plot analysis of NSNSRE
-        plot_analysis_of_NSNRE(nreSettings=nreSettings, network_storage=network_storage,
                                samples_storage=samples_storage, dkl_storage=dkl_storage,
                                obs=obs, true_posterior=mcmc_true, root=nreSettings.root)
     logger.info('Finished')
