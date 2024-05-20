6f494f95f1443cf7f2aefa9f3a4bd4a515b9d1be
kilian1103 <kilian1103hotmail.de>
2024-05-20 20:41:18 +0100

    refactor shuffling of training dataa

--- NSLFI/NRE_retrain.py
+++ NSLFI/NRE_retrain.py
@@ -52,16 +52,18 @@ def retrain_next_round(root: str, deadpoints: np.ndarray, nreSettings: NRE_Setti
         else:
             sample = sim.sample(conditions=cond, targets=[nreSettings.obsKey])
             samples.append(sample)
+
+    comm_gen.Barrier()
     samples = comm_gen.allgather(samples)
     samples = np.concatenate(samples, axis=0)
     samples = samples.tolist()
     logger.info(f"Total number of samples for training the network: {len(samples)}")
-    samples = sklearn.utils.shuffle(samples)
     comm_gen.Barrier()
     samples = reformat_samples(samples)
     logger.info("Simulation done!")
 
-    if nreSettings.continual_learning_mode and rd > 0:
+    ### concatenate previous training data with new training data###
+    if rd > 0:
         previous_root = f"{nreSettings.root}/{nreSettings.child_root}_{rd - 1}"
         try:
             previous_samples = torch.load(f=f"{previous_root}/{nreSettings.joint_training_data_fileroot}")
@@ -74,6 +76,19 @@ def retrain_next_round(root: str, deadpoints: np.ndarray, nreSettings: NRE_Setti
         Ds = np.concatenate((previous_samples[nreSettings.obsKey], samples[nreSettings.obsKey]), axis=0)
         del previous_samples
         samples = {nreSettings.targetKey: thetas, nreSettings.obsKey: Ds}
+
+    ### shuffle training data to reduce ordinal bias introduced by deadpoints###
+    if rank_gen == 0:
+        thetas = samples[nreSettings.targetKey]
+        Ds = samples[nreSettings.obsKey]
+        joint =  np.array(sklearn.utils.shuffle(list(zip(thetas,Ds))),dtype=object)
+        thetas = np.stack(joint[:, 0])
+        Ds = np.stack(joint[:, 1])
+        del joint
+        samples = {nreSettings.targetKey: thetas, nreSettings.obsKey: Ds}
+
+    comm_gen.Barrier()
+    samples = comm_gen.bcast(samples, root=0)
 
     ### save training data for NRE on disk ###
     if nreSettings.save_joint_training_data and rank_gen == 0:
